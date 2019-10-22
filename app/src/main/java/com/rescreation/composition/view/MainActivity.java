1d03f8e45b71f148880f3d39fff320bcc0190de3
elahee <srelahee@gmail.com>
2019-10-22 14:05:34 +0600

    Initial commit

---
+++ app/src/main/java/com/rescreation/composition/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rescreation.composition.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rescreation.composition.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
